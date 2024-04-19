f428c9e3a8043cd64a9227913dbfd49987b26844
Anish Gautam <anishgautam2024@gmail.com>
2024-04-19 11:05:41 +0545

    commit

--- src/components/Pages/About/DataCard.jsx
+++ src/components/Pages/About/DataCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DataCard = ({ data }) => {
   return (
-    <div className="w-[100%] max-h-[170px] sm:max-h-[200px] border border-solid
+    <div className="w-[100%] max-h-[170px] sm:max-h-[200px] overflow-hidden overflow-y-auto border border-solid
      border-[#353535] rounded-lg text-gray-400 flex gap-[10px] items-start sm:gap-[20px] ]
       mb-[15px] sm:mb-[30px] p-[10px] sm:p-[20px] ">
       <div className='mt-[6px]'>
