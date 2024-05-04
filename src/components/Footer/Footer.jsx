a61a05db55ab8b0f9b2906b73a818883e5e444ef
Anish Gautam <anishgautam2024@gmail.com>
2024-05-04 19:20:11 +0545

    updated github code link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='sm:mt-[10px] text-center sm:text-[22px] font-semibold '>
-      Source Code : <a href="https://github.com/Anish570/Sub-Portofolio.git" className='text-blue-600 hover:text-cyan-400 cursor-pointer'>Github</a>
+      Source Code : <a href="https://github.com/Anish570/Sub-Portfolio.git" className='text-blue-600 hover:text-cyan-400 cursor-pointer'>Github</a>
     </div>
   )
 }
